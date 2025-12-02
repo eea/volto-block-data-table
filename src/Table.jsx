f28a76044562fcd0956bd44186dd5d076b5f74ae
Miu Razvan <miu.razvan28@gmail.com>
2025-12-02 13:30:32 +0200

    feat(transpose): Add transpose table option, ref #294569

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import cx from 'classnames';
 import { Table } from 'semantic-ui-react';
 import config from '@plone/volto/registry';
@@ -7,11 +7,54 @@ import './table.less';
 
 const TableComponent = (props) => {
   const { components } = config.settings.table;
-  const { rows, columnDefs, defaultColDef, withHeaders, TableProps } = props;
+  const {
+    rows,
+    columnDefs,
+    defaultColDef,
+    withHeaders,
+    transposed,
+    TableProps,
+  } = props;
   const { celled, striped, borderless, compacted } = TableProps || {};
 
   const [ColumnHeader] = useState(() => components.columnHeader);
   const [CellRenderer] = useState(() => components.cellRenderer);
+
+  // Transposed view: each column becomes a row
+  if (transposed) {
+    return (
+      <Table
+        className={cx('data-table unstackable transposed', {
+          borderless,
+          compacted,
+        })}
+        celled={celled}
+        striped={striped}
+        stackable={false}
+      >
+        <Table.Body>
+          {columnDefs.map((colDef) => (
+            <Table.Row key={colDef.field}>
+              {withHeaders && (
+                <ColumnHeader
+                  className="th"
+                  colDef={{ ...defaultColDef, ...colDef }}
+                  as={Table.Cell}
+                />
+              )}
+              {rows.map((row) => (
+                <CellRenderer
+                  key={row.id}
+                  colDef={{ ...defaultColDef, ...colDef }}
+                  row={row}
+                />
+              ))}
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    );
+  }
 
   return (
     <Table
